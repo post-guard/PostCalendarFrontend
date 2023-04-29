4618cfb7233920d72709550a38a7ae7647af10d3
jackfiled <xcrenchangjun@outlook.com>
2023-04-29 12:13:51 +0800

    add: fabric绘图库

---
+++ src/components/MapEditor.vue
@@ -0,0 +1,30 @@
+<template>
+    <canvas ref="map" width="auto" height="auto">
+        你的浏览器不支持canvas，请升级浏览器版本~
+    </canvas>
+</template>
+
+<script setup lang="ts">
+import { Canvas, Image } from "fabric";
+import { onMounted, ref } from "vue";
+
+
+const map = ref<HTMLCanvasElement>();
+
+onMounted( async () => {
+    if (map.value != undefined) {
+        const canvas = new Canvas(map.value);
+
+        const background = await Image.fromURL("xtcmap.svg");
+
+        background.scale(canvas.height / background.height);
+
+        background.selectable = false;
+        canvas.add(background);
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
