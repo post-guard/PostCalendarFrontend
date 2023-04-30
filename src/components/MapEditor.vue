3fc7326aa32f95ebbdada6b2e8c7dc1ba34ff25f
Ichirinko <1621543655@qq.com>
2023-04-30 10:03:57 +0800

    add:限制了缩放的范围 只做了一点小小的工作

--- src/components/MapEditor.vue
+++ src/components/MapEditor.vue
@@ -31,12 +31,13 @@ onMounted( async () => {
         background.hasControls = false;
         background.hasBorders =  true;
         background.lockRotation = true;
+        background.left = Math.abs(canvas.width-background.getScaledWidth())/2;
 
         canvas.add(background);
 
 
         background.on('mousewheel',e=>zoom(e,canvas,background));
-
+//todo：即将完成地图按鼠标为原点缩放，限制拖拽范围，标志点的坐标计算与添加/删除功能
     }
 })
 
@@ -73,34 +74,47 @@ async function createMap(){
 function zoom(event: any,canvas:Canvas,background:Image){
 
 
+    const scaleX = background.getScaledWidth()
+    const preScale = scaleX/background.width;//原比例
 
-    if(canvas!=undefined){
-
-        console.log(event.e.deltaY)
 
         if(event.e.deltaY<0){
-            console.log("j")
-            const scaleX = background.getScaledWidth()
-            const preScale = scaleX/background.width;
 
+            let afterScale = preScale - 0.01;
 
+            afterScale = proportionLimit(afterScale,0.19093758905671132,0.7509375890567117)
+            //这两串神奇的数字问就是手算的
+            background.scale(afterScale);
 
-            background.scale(preScale-0.1);
-            //background.set('scale',preScale-0.1)
             canvas.renderAll()
         }
         else if(event.e.deltaY>0){
-            const scaleX = background.getScaledWidth()
-            const preScale = scaleX/background.width;
 
-            background.scale(preScale+0.1);
-            //background.set('scale',preScale+0.1)
+            let afterScale = preScale + 0.01;
+
+            afterScale = proportionLimit(afterScale,0.19093758905671132,0.7509375890567117)
+            //这两串神奇的数字问就是手算的
+            background.scale(afterScale);
+
             canvas.renderAll()
         }
-    }
+
 
 }
 
+function proportionLimit(value:number,min:number,max:number):number{
+
+    if(value>max){
+        value = max;
+    }
+    else if(value<min){
+        value = min;
+    }
+    else{
+
+    }
+    return value;
+}
 
 </script>
 
